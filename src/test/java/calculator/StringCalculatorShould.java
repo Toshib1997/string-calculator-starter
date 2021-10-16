3478923f287cf4989621e0d3a509f273ada31914
Toshib1997 <toshibsanadi9738@gmail.com>
2021-10-17 01:05:07 +0530

    tdd 5

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -43,6 +43,14 @@ public class StringCalculatorShould {
         assertEquals(stringCalculator.add("1\n2"), 3);
         assertEquals(stringCalculator.add("11\n13"), 24);
     }
+    
+    
+    @Test
+    public void threeNumbersDelimitedAnywayShouldBeSummed() {
+    	 StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(stringCalculator.add("1,2,3"), 6);
+        assertEquals(stringCalculator.add("5\n2\n3"), 10);
+    }
 
 
 }
